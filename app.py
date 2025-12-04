329d1cf0692eb326a9c389c2a1ff9a1590b6ba3e
Thomas Edward Gladwin <thomas.gladwin@gmail.com>
2025-12-04 19:37:43 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -9,50 +9,39 @@ verbose0 = False
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    global conversation_memory
-    debug_str = ''
-    query = ''
-    if 'Forget' in request.form:
-        if request.form["Forget"] == 'valForget':
-            debug_str = debug_str + 'Memory wiped. '
-            conversation_memory = []
+    return render_template('index.html')
+
+@app.route('/relevanceScans', methods=['GET', 'POST'])
+def relevanceScans():
+    response = 'Not executed (mode)'
     if 'ThomAIs' in request.form:
-        if request.form["ThomAIs"] == 'valThomAIs':
-            funcs.ThomAIs_on = funcs.ThomAIs_on == False
-            if funcs.ThomAIs_on:
-                debug_str = debug_str + 'ThomAIs activated. '
-            else:
-                debug_str = debug_str + 'ThomAIs deactivated. '
-    if 'query' in request.form:
-        definitions = request.form['definitions']
-        data = request.form['data']
-        query = request.form['query']
-        if len(query) == 0:
-            response = "No query."
-        else:
-            try:
-                input = funcs.create_input(query, conversation_memory, definitions, data)
-                if len(input) > 0:
-                    response = funcs.query_AI(input)
-                    conversation_memory.append({'role': "user", 'content': query})
-                    conversation_memory.append({'role': "assistant", 'content': response})
-                else:
-                    response = "Error - possibly too much data for guest account."
-                query = ''
-            except:
-                response = "An error occurred while accessing the AI model - please try again."
-            debug_str = debug_str + query + ". "
-    else:
-        response = "Waiting for query."
-        definitions = ''
-        data = ''
-    if verbose0:
-        response = "Debug info: " + debug_str + ". " + response
-    if funcs.ThomAIs_on:
-        ThomAIs_onoffStr = "On"
-    else:
-        ThomAIs_onoffStr = "Off"
-    return render_template('index.html', query=query, response=response, definitions=definitions, data=data, ThomAIs_onoff=ThomAIs_onoffStr)
+        if request.form["ThomAIs"] == '1':
+            if 'query' in request.form:
+                query = request.form['query']
+                if len(query) > 0:
+                    try:
+                        response_list = funcs.fThomasAIs_relevanceScans(query)
+                        response = str(response_list)
+                    except:
+                        response = "Not executed (query)"
+    jsonResp = {"response": response}
+    return jsonify(jsonResp)
+
+@app.route('/setThomasContent', methods=['GET', 'POST'])
+def setThomasContent():
+    response = ''
+    if 'ThomAIs' in request.form:
+        if request.form["ThomAIs"] == '1':
+            if 'query' in request.form:
+                query = request.form['query']
+                if len(query) > 0:
+                    try:
+                        funcs.fThomasAIs_createInput(query)
+                        response = "1"
+                    except:
+                        response = "0"
+    jsonResp = {"response": response}
+    return jsonify(jsonResp)
 
 @app.route('/fetch_info', methods=['GET', 'POST'])
 def f_fetch():
@@ -63,13 +52,11 @@ def f_fetch():
         if request.form["Forget"] == '1':
             debug_str = debug_str + 'Memory wiped. '
             conversation_memory = []
+    funcs.ThomAIs_on = False
     if 'ThomAIs' in request.form:
         if request.form["ThomAIs"] == '1':
-            funcs.ThomAIs_on = funcs.ThomAIs_on == False
-            if funcs.ThomAIs_on:
-                debug_str = debug_str + 'ThomAIs activated. '
-            else:
-                debug_str = debug_str + 'ThomAIs deactivated. '
+            funcs.ThomAIs_on = True
+            debug_str = debug_str + 'ThomAIs activated. '
     if 'query' in request.form:
         try:
             definitions = request.form['definitions']
--- funcs.py
+++ funcs.py
@@ -12,19 +12,24 @@ client = OpenAI(
 
 ThomAIs_on = False
 guest = True
+ThomasContent = ''
+relevantKeys = []
 
 def create_input(query, memory0=None, definitions="", data=""):
+    global ThomasContent
     input = [
         {"role": "user", "content": "I am a researcher and need accurate and considerate responses."},
         {"role": "assistant", "content": "I will provide accurate amd nuanced responses, focused on being based on clear evidence and referring to any relevant theories or data."},
     ]
     if ThomAIs_on:
-        ThomasContent = fThomasAIs(query)
         if len(ThomasContent) > 0:
             input.append({"role": "user",
                                         "content": "Be aware of the following ideas, findings, references, and arguments; use their style of speaking and argumentation: " + ThomasContent})
             input.append({"role": "assistant",
-                                    "content": "I will use these ideas where relevant, and will follow the style of thinking and argumentation."})
+                                    "content": "I will use these ideas where relevant, and will follow the style of thinking and argumentation. I will talkabout them as if they were my papers, essays, or thinking"})
+    if len(definitions) > 0:
+        input.append({"role": "user", "content": "Use the following definitions of terms: " + definitions})
+        input.append({"role": "assistant", "content": "I will make sure to apply these definitions in any relevant responses."})
     if len(data) > 0:
         input.append({"role": "user", "content": "Use the following information where relevant when answering questions: " + data})
         input.append({"role": "assistant", "content": "I will check whether this information is relevant and use it if so."})
@@ -32,9 +37,6 @@ def create_input(query, memory0=None, definitions="", data=""):
         for mem0 in memory0:
             if len(mem0["content"]) > 0 and not mem0 is None:
                 input.append(mem0)
-    if len(definitions) > 0:
-        input.append({"role": "user", "content": "Use the following definitions of terms: " + definitions})
-        input.append({"role": "assistant", "content": "I will make sure to apply these definitions in any relevant responses."})
     input.append({"role": "user", "content": query})
     return input
 
@@ -53,33 +55,43 @@ def query_AI(input):
     )
     return response.output_text
 
-def fThomasAIs(query):
-    ThomasContent = ''
+def fThomasAIs_relevanceScans(query):
+    global relevantKeys
+    relevantKeys = []
     topics = BeliefsAndOpinions.Abstr.keys()
     for topic in topics:
-        ThomasContent += BeliefsAndOpinions.Full[topic]
-        timeToRunThis = False
-        if timeToRunThis:
-            abstr = BeliefsAndOpinions.Abstr[topic]
+        abstr = BeliefsAndOpinions.Abstr[topic]
+        input = [
+            {"role": "user", "content": "Respond with only a digit from 0 to 10. On a scale from 0 to 9, with 0 being not at all relevant and 9 being extremely relevant, how relevant is the following text to the query, " + query + ": " + abstr}
+        ]
+        response = query_AI(input)
+        try:
+            digits = re.findall(r'\d+', response)
+            if len(digits) > 0:
+                score = int(digits[0])
+            else:
+                score = 9
+        except:
+            score = 9
+        #print(response + " " + str(score))
+        if score > 1:
+            relevantKeys.append(topic)
+    return relevantKeys
+
+def fThomasAIs_createInput(query):
+    global ThomasContent
+    global relevantKeys
+    ThomasContent = ''
+    for topic in relevantKeys:
+        full = BeliefsAndOpinions.Full[topic]
+        useQueriedSummmary = False
+        if useQueriedSummmary:
             input = [
-                {"role": "user", "content": "Respond with only a digit from 0 to 10. On a scale from 0 to 9, with 0 being not at all relevant and 9 being extremely relevant, how relevant is the following text to the query, " + query + ": " + abstr}
+                {"role": "user",
+                 "content": "Summarize, reporting where relevant the content and academic references given in the following text. in at most 500 words what is relevant from the following text to the query, " + query + ": " + full}
             ]
             response = query_AI(input)
-            try:
-                digits = re.findall(r'\d+', response)
-                if len(digits) > 0:
-                    score = int(digits[0])
-                else:
-                    score = 9
-            except:
-                score = 9
-            #print(response + " " + str(score))
-            if score > 1:
-                full = BeliefsAndOpinions.Full[topic]
-                input = [
-                    {"role": "user",
-                     "content": "Summarize, reporting where relevant the content and academic references given in the following text. in at most 500 words what is relevant from the following text to the query, " + query + ": " + full}
-                ]
-                response = query_AI(input)
-                ThomasContent += response
+            ThomasContent += response
+        else:
+            ThomasContent += full
     return ThomasContent
