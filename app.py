5077803318f8e21a29115dd2413510ff488fc9f9
Thomas Edward Gladwin <thomas.gladwin@gmail.com>
2025-12-14 22:37:18 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -1,20 +1,36 @@
 from flask import Flask, render_template, request, jsonify
-import funcs
+import funcs as funcsLib
 import time
 
 app = Flask(__name__)
 
-conversation_memory = []
 verbose0 = False
 queue_fn = "streaming.txt"
+interface_list = {}
+timeout_queue = 120
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
+@app.route('/init', methods=['GET', 'POST'])
+def init():
+    ID = request.form["ID"]
+    interface_list[ID] = funcsLib.AIInterface()
+    setInUse()
+    return ""
+
+@app.route('/cleanup', methods=['GET', 'POST'])
+def cleanup():
+    ID = request.form["ID"]
+    interface_list.pop(ID, None)
+    setNotInUse()
+    return ""
+
 @app.route('/relevanceScans', methods=['GET', 'POST'])
 def relevanceScans():
+    funcs = interface_list[request.form["ID"]]
     response = 'Not executed (mode)'
     if 'ThomAIs' in request.form:
         if request.form["ThomAIs"] == '1':
@@ -26,12 +42,15 @@ def relevanceScans():
                         response = str(response_list)
                     except:
                         response = "Not executed (query)"
+    if len(response) == 0:
+        response = " "
     jsonResp = {"response": response}
     return jsonify(jsonResp)
 
 @app.route('/setThomasContent', methods=['GET', 'POST'])
 def setThomasContent():
-    response = ''
+    funcs = interface_list[request.form["ID"]]
+    response = ' '
     if 'ThomAIs' in request.form:
         if request.form["ThomAIs"] == '1':
             if 'query' in request.form:
@@ -42,18 +61,20 @@ def setThomasContent():
                         response = "1"
                     except:
                         response = "0"
+    if len(response) == 0:
+        response = " "
     jsonResp = {"response": response}
     return jsonify(jsonResp)
 
 @app.route('/fetch_info', methods=['GET', 'POST'])
 def f_fetch():
-    global conversation_memory
+    funcs = interface_list[request.form["ID"]]
     debug_str = ''
     query = ''
     if 'Forget' in request.form:
         if request.form["Forget"] == '1':
             debug_str = debug_str + 'Memory wiped. '
-            conversation_memory = []
+            funcs.conversation_memory = None
     funcs.ThomAIs_on = False
     if 'ThomAIs' in request.form:
         if request.form["ThomAIs"] == '1':
@@ -71,14 +92,14 @@ def f_fetch():
             response = "No query."
         else:
             try:
-                input = funcs.create_input(query, conversation_memory, definitions, data)
+                input = funcs.create_input(query, definitions, data)
                 if len(input) > 0:
                     try:
                         response = funcs.query_AI_stream(input)
                     except:
                         response = "Error at query_AI"
-                    conversation_memory.append({'role': "user", 'content': query})
-                    # conversation_memory.append({'role': "assistant", 'content': response})
+                    funcs.conversation_memory.append({'role': "user", 'content': query})
+                    # funcs.conversation_memory.append({'role': "assistant", 'content': response})
                 else:
                     response = "Error - possibly too much data for guest account."
                 query = ''
@@ -98,18 +119,18 @@ def f_fetch():
 
 @app.route('/check_stream', methods=['GET', 'POST'])
 def check_stream():
-    global conversation_memory
     completed_str = "0"
     response = ""
     debug_str = ''
     try:
+        funcs = interface_list[request.form["ID"]]
         n_tokens = 10
         for s in funcs.streaming_response:
             debug_str += s.type
             if "response.output_text.done" in s.type:
                 completed_str = "1"
                 response = s.text
-                conversation_memory.append({'role': "assistant", 'content': response})
+                funcs.conversation_memory.append({'role': "assistant", 'content': response})
                 break
             if hasattr(s, 'delta'):
                 funcs.streaming_str += s.delta
@@ -118,13 +139,14 @@ def check_stream():
                 response = funcs.streaming_str
                 break
     except:
-        response = ""
-    # response += debug_str
+        pass
+    #response += debug_str
     jsonResp = {"response": response, "completed": completed_str}
     return jsonResp
 
 @app.route('/relevanceScansLoop', methods=['GET', 'POST'])
 def relevanceScansLoop():
+    funcs = interface_list[request.form["ID"]]
     if 'ThomAIs' not in request.form:
         jsonResp = {"response": "1", "rel_id": "-1", "score": "-1"}
         return jsonResp
@@ -132,18 +154,17 @@ def relevanceScansLoop():
         jsonResp = {"response": "1", "rel_id": "-1", "score": "-1"}
         return jsonResp
     query = request.form['query']
-    if funcs.rel_id >= len(funcs.BeliefsAndOpinions.Abstr):
+    if funcs.rel_id >= len(funcsLib.BeliefsAndOpinions.Abstr):
         funcs.fThomasAIs_createInput(query)
         funcs.relevanceScans_init()
-        score = 0
+        score = "0"
         response = "1"
     else:
         score = funcs.relevanceScans_iter(query)
         response = "0"
-    jsonResp = {"response": response, "rel_id": str(funcs.rel_id - 1), "score": str(score)}
+    jsonResp = {"response": response, "rel_id": str(funcs.rel_id - 1), "score": score}
     return jsonResp
 
-@app.route('/setInUse', methods=['GET', 'POST'])
 def setInUse():
     try:
         with open(queue_fn, "a") as text_file:
@@ -153,7 +174,6 @@ def setInUse():
         pass
     return ""
 
-@app.route('/setNotInUse', methods=['GET', 'POST'])
 def setNotInUse():
     try:
         with open(queue_fn, "a") as text_file:
@@ -164,7 +184,7 @@ def setNotInUse():
 
 @app.route('/check_queue', methods=['GET', 'POST'])
 def check_queue():
-    if request.form["accesscode"] in funcs.AccessCodes:
+    if request.form["accesscode"] in funcsLib.AccessCodes:
         status = "0"
     else:
         status = "0"
@@ -178,11 +198,13 @@ def check_queue():
                     if status != "0":
                         last_timestamp = float(status)
                         current_timestamp = time.time()
-                        if (current_timestamp - last_timestamp) > 30:
+                        if (current_timestamp - last_timestamp) > timeout_queue:
                             status = "0"
                         else:
                             status = "1"
         except:
-            pass
-    jsonResp = {"response": status[-1], "access": request.form["accesscode"]}
+            status = "0"
+    if len(status) == 0:
+        status = " "
+    jsonResp = {"response": status}
     return jsonResp
--- funcs.py
+++ funcs.py
@@ -7,82 +7,100 @@ import base64
 import BeliefsAndOpinions
 import re
 
-client = OpenAI(
-    api_key="xxx"
-)
+api_key = "xxx"
 AccessCodes = ["xxx"]
 
-ThomAIs_on = False
-guest = True
-ThomasContent = ''
-relevantKeys = []
-rel_id = 0
-streaming_response = None
-streaming_str = ''
-
-def create_input(query, memory0=None, definitions="", data=""):
-    global ThomasContent
-    global ThomAIs_on
-    input = [
-        {"role": "user", "content": "I am a researcher and need accurate and considerate responses. However, always answer quickly enough."},
-        {"role": "assistant", "content": "I will answer within 18 seconds. Given that, I will provide accurate and nuanced responses, focused on being based on clear evidence and referring to any relevant theories or data."},
-    ]
-    if ThomAIs_on:
-        if len(ThomasContent) > 0:
-            input.append({"role": "user",
-                                        "content": "Be aware of the following ideas, findings, references, and arguments; refer to them where relevant to the query; use their style of speaking and argumentation: " + ThomasContent})
-            input.append({"role": "assistant",
-                                    "content": "I will use these ideas where relevant, and will follow the style of thinking and argumentation. I will talk about them as if they were my papers, essays, or thinking"})
-    if len(definitions) > 0:
-        input.append({"role": "user", "content": "Use the following definitions of terms: " + definitions})
-        input.append({"role": "assistant", "content": "I will make sure to apply these definitions in any relevant responses."})
-    if len(data) > 0:
-        input.append({"role": "user", "content": "Use the following information where relevant when answering questions: " + data})
-        input.append({"role": "assistant", "content": "I will check whether this information is relevant and use it if so."})
-    if not memory0 is None:
-        for mem0 in memory0:
-            if len(mem0["content"]) > 0 and not mem0 is None:
-                input.append(mem0)
-    input.append({"role": "user", "content": query})
-    return input
-
-def query_AI_chat(input):
-    response = client.chat.completions.create(
-        model="gpt-5",
-        messages=input,
-        max_completion_tokens=1000
-    )
-    return response.choices[0].message.content
-
-def query_AI(input):
-    response = client.responses.create(
-        model="gpt-5",
-        input=input
-    )
-    return response.output_text
-
-def query_AI_stream(input):
-    global streaming_response
-    global streaming_str
-    streaming_str = ''
-    response = client.responses.create(
-        model="gpt-5",
-        input=input,
-        stream=True
-    )
-    streaming_response = response
-    return "Streaming initiated, waiting for responses..."
-
-def fThomasAIs_relevanceScans(query):
-    global relevantKeys
+class AIInterface():
+    ThomAIs_on = False
+    guest = True
+    ThomasContent = ''
     relevantKeys = []
-    topics = BeliefsAndOpinions.Abstr.keys()
-    for topic in topics:
+    rel_id = 0
+    streaming_response = None
+    streaming_str = ''
+    conversation_memory = []
+
+    client = OpenAI(
+        api_key=api_key
+    )
+
+    def create_input(self, query, definitions="", data=""):
+        input = [
+            {"role": "user", "content": "I am a researcher and need accurate and considerate responses. However, always answer quickly enough."},
+            {"role": "assistant", "content": "I will answer within 18 seconds. Given that, I will provide accurate and nuanced responses, focused on being based on clear evidence and referring to any relevant theories or data."},
+        ]
+        if self.ThomAIs_on:
+            if len(self.ThomasContent) > 0:
+                input.append({"role": "user",
+                                            "content": "Be aware of the following ideas, findings, references, and arguments; refer to them where relevant to the query; use their style of speaking and argumentation: " + self.ThomasContent})
+                input.append({"role": "assistant",
+                                        "content": "I will use these ideas where relevant, and will follow the style of thinking and argumentation. I will talk about them as if they were my papers, essays, or thinking"})
+        if len(definitions) > 0:
+            input.append({"role": "user", "content": "Use the following definitions of terms: " + definitions})
+            input.append({"role": "assistant", "content": "I will make sure to apply these definitions in any relevant responses."})
+        if len(data) > 0:
+            input.append({"role": "user", "content": "Use the following information where relevant when answering questions: " + data})
+            input.append({"role": "assistant", "content": "I will check whether this information is relevant and use it if so."})
+        if len(self.conversation_memory) > 0:
+            for mem0 in self.conversation_memory:
+                if len(mem0["content"]) > 0 and not mem0 is None:
+                    input.append(mem0)
+        input.append({"role": "user", "content": query})
+        return input
+
+    def query_AI_chat(self, input):
+        response = self.client.chat.completions.create(
+            model="gpt-5",
+            messages=[input],
+            max_completion_tokens=1000
+        )
+        return response.choices[0].message.content
+
+    def query_AI(self, input):
+        response = self.client.responses.create(
+            model="gpt-5",
+            input=input
+        )
+        return response.output_text
+
+    def query_AI_stream(self, input):
+        self.streaming_str = ''
+        response = self.client.responses.create(
+            model="gpt-5",
+            input=input,
+            stream=True
+        )
+        self.streaming_response = response
+        return "Streaming initiated, waiting for responses..."
+
+    def fThomasAIs_createInput(self, query):
+        self.ThomasContent = ''
+        for topic in self.relevantKeys:
+            full = BeliefsAndOpinions.Full[topic]
+            useQueriedSummmary = False
+            if useQueriedSummmary:
+                input = [
+                    {"role": "user",
+                     "content": "Summarize, reporting where relevant the content and academic references given in the following text, in at most 750 words what is relevant from the following text to the query, " + query + ": " + full}
+                ]
+                response = self.query_AI(input)
+                self.ThomasContent += response
+            else:
+                self.ThomasContent += full
+        return self.ThomasContent
+
+    def relevanceScans_init(self):
+        self.relevantKeys = []
+        self.rel_id = 0
+
+    def relevanceScans_iter(self, query):
+        topics = BeliefsAndOpinions.Abstr.keys()
+        topic = list(topics)[self.rel_id]
         abstr = BeliefsAndOpinions.Abstr[topic]
         input = [
             {"role": "user", "content": "Respond with only a digit from 0 to 10. On a scale from 0 to 9, with 0 being not at all relevant and 9 being extremely relevant, how relevant is the following text to the query, " + query + ": " + abstr}
         ]
-        response = query_AI(input)
+        response = self.query_AI(input)
         try:
             digits = re.findall(r'\d+', response)
             if len(digits) > 0:
@@ -93,71 +111,6 @@ def fThomasAIs_relevanceScans(query):
             score = 9
         #print(response + " " + str(score))
         if score > 1:
-            relevantKeys.append(topic)
-    return relevantKeys
-
-def fThomasAIs_createInput(query):
-    global ThomasContent
-    global relevantKeys
-    ThomasContent = ''
-    for topic in relevantKeys:
-        full = BeliefsAndOpinions.Full[topic]
-        useQueriedSummmary = False
-        if useQueriedSummmary:
-            input = [
-                {"role": "user",
-                 "content": "Summarize, reporting where relevant the content and academic references given in the following text, in at most 750 words what is relevant from the following text to the query, " + query + ": " + full}
-            ]
-            response = query_AI(input)
-            ThomasContent += response
-        else:
-            ThomasContent += full
-    return ThomasContent
-
-def relevanceScans_init():
-    global relevantKeys
-    global rel_id
-    relevantKeys = []
-    rel_id = 0
-
-def relevanceScans_iter(query):
-    global relevantKeys
-    global rel_id
-    topics = BeliefsAndOpinions.Abstr.keys()
-    topic = list(topics)[rel_id]
-    abstr = BeliefsAndOpinions.Abstr[topic]
-    input = [
-        {"role": "user", "content": "Respond with only a digit from 0 to 10. On a scale from 0 to 9, with 0 being not at all relevant and 9 being extremely relevant, how relevant is the following text to the query, " + query + ": " + abstr}
-    ]
-    response = query_AI(input)
-    try:
-        digits = re.findall(r'\d+', response)
-        if len(digits) > 0:
-            score = int(digits[0])
-        else:
-            score = 9
-    except:
-        score = 9
-    #print(response + " " + str(score))
-    if score > 1:
-        relevantKeys.append(topic)
-    rel_id += 1
-    return score
-
-def generate_image(prompt):
-    prompt = """
-    A children's book drawing of a veterinarian using a stethoscope to 
-    listen to the heartbeat of a baby otter.
-    """
-
-    result = client.images.generate(
-        model="gpt-image-1",
-        prompt=prompt
-    )
-
-    image_base64 = result.data[0].b64_json
-    image_bytes = base64.b64decode(image_base64)
-
-    # Save the image to a file
-    with open("image.png", "wb") as f:
-        f.write(image_bytes)
+            self.relevantKeys.append(topic)
+        self.rel_id += 1
+        return response # str(score)
